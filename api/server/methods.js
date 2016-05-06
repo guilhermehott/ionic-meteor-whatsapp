Urigo/IonicCLI-Meteor-WhatsApp

Step 4.23: Add new message method to api

api/server/methods.js
@@ -1,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Cahts, Messages } from './collections';
+
+Meteor.methods({
+  newMessage(message) {
+    message.timestamp = new Date();
+
+    const messageId = Messages.insert(message);
+    Chats.update(message.chatId, { $set: { lastMessage: message } });
+
+    return messageId;
+  }
+});

guilhermehott/ionic-meteor-whatsapp

api/server/methods.js
@@ -1,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Chats, Messages } from './collections';
+
+Meteor.methods({
+  newMessage(message) {
+    message.timestamp = new Date();
+
+    const messageId = Messages.insert(message);
+    Chats.update(message.chatId, { $set: { lastMessage: message } });
+
+    return messageId;
+  }
+});